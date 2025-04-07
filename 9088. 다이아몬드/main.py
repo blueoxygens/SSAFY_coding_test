8c873daaabfe3ef6ff861fdcaaf487d665f9777e
blueoxygens <exboxer234@gmail.com>
2025-04-08 02:03:03 +0900

    feat: 다이아몬드 문제 풀이.. 파이썬 문제가 아니라 확인 불가

---
+++ 9088. 다이아몬드/main.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.stdin = open("sample_input.txt", "r")
+
+TC = int(input())
+
+for T in range(1, TC+1):
+    N, K = map(int, input().strip().split(" "))
+    arr = []
+    for i in range(N):
+        arr.append(int(input()))
+
+    arr = sorted(arr)
+    l = len(arr)
+    max = 0
+    for i in range(l, 0, -1):
+        for j in range(l-1):
+            if j + i > l-1 :
+                break
+            if arr[j+i]-arr[j] > K:
+                break
+            if i > max:
+                max = i
+    print(f'#{T} {max+1}')
